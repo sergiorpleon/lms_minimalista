ae50ed20f4dedb28d7eb1a5b8a7942e990499415
sergio <sergiorpleon@gmail.com>
2026-02-01 00:30:12 +0100

    add try catch

--- final/wsgi.py
+++ final/wsgi.py
@@ -13,5 +13,9 @@ from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'final.settings')
 
-application = get_wsgi_application()
-app = application
+try:
+    application = get_wsgi_application()
+    app = application # <-- El alias para Vercel
+except Exception as e:
+    print(f"Error cargando la aplicacion: {e}")
+    raise e
